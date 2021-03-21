928df65e504540725ab1eab46201696b53dc1570
CJF86 <77642189+CJF86@users.noreply.github.com>
2021-03-21 14:30:48 -0400

    Third check-in

--- Encryption.cs
+++ Encryption.cs
@@ -18,7 +18,7 @@ namespace ArrayProject
         public Encryption(string Plaintext_Message,int Encrypt_Key) //creates encryption method for encryption
         {
             //Creates character based array filled with letters of the alphabet
-            char[] Alphabet_Constant = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'Z' };
+            char[] Alphabet_Constant = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
             char[] Hidden_Message = Plaintext_Message.ToCharArray();
 
@@ -26,11 +26,16 @@ namespace ArrayProject
 
             for(int i = 0; i < Plaintext_Message.Length; i++)
             {
-                int Index_Value = Array.IndexOf(Alphabet_Constant, Hidden_Message); //provides the index value of the character of the hidden message within the alphabet
+                int Index_Value = Array.IndexOf(Alphabet_Constant, Hidden_Message[i]); //provides the index value of the character of the hidden message within the alphabet
 
-                int Shifted_Index = (Index_Value += User_Key) % 26; //provides the shifted index value based on the user key
+                //Array.IndexOf must not be finding the values within Alphabet_Constant and is thus returning -1 causing the exception error
+                //It is for some reason not doing any of the math within the loop to edit the index vale
 
-                char Encrypted_Character = Alphabet_Constant[Shifted_Index];//problem is here
+                int Shifted_Index = (Index_Value + Encrypt_Key) % 26; //provides the shifted index value based on the user key
+
+                Console.WriteLine(Shifted_Index); //Should write the value of the index for each iteration
+
+                char Encrypted_Character = Alphabet_Constant[Shifted_Index];
 
                 Encrypted_Message[i] = Encrypted_Character;
             }
